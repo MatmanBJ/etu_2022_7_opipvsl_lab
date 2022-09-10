ea0b0d3f8ba738dcf7c2997424c846d3ed5d972c
MatmanBJ <matmanbj@gmail.com>
2022-09-10 14:04:54 +0300

    lab_1 program v2

--- Laboratory_work_1/lab_1/main.cpp
+++ Laboratory_work_1/lab_1/main.cpp
@@ -4,6 +4,9 @@ using namespace std;
 
 int main()
 {
-	cout << "Hello, World!";
+	cout << "Hello, World!\n\n";
+	cout << "Course: Organization of process and programming in Linux enviroment\n";
+	cout << "Group: 9308\n";
+	cout << "Full name: Sobolev Matvey Sergeevich\n";
 	return 0;
 }
