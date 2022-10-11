4631235d4b9bf8bbb9f6dd52457f4071b469114f
MatmanBJ <matmanbj@gmail.com>
2022-10-12 00:36:43 +0300

    lab_6 program v2

--- Laboratory_work_6/lab_6/main.cpp
+++ Laboratory_work_6/lab_6/main.cpp
@@ -1,10 +1,7 @@
 #include <iostream>
-#include <stdlib.h>
 #include <signal.h>
 #include <sys/wait.h>
 #include <sys/time.h>
-#include <time.h>
-#include <termios.h>
 #include <unistd.h>
 
 using namespace std;
@@ -12,38 +9,39 @@ using namespace std;
 void processFunction (int local_int); // function for repeat
 float finalTime (float local_start_time); // counting final time
 
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
-	int period = 0; // launch period, 0 by default
-	int numOfL = 0; // number of launches, 0 by default
-	float start_time; // child start time
-	struct itimerval cur;
-	struct itimerval old;
-	struct sigaction sig;
+	int number_period = 0; // launch period, 0 by default
+	int number_launch = 0; // number of launches, 0 by default
+	float start_time; // parent process start time
+	struct itimerval value; // for timer structure, new time set
+	struct itimerval old_value; // for timer structure, old timer dropped here
+	struct sigaction sigact;
+	
+	number_period = atoi(argv[1]); // convert input data to numeric
+	number_launch = atoi(argv[2]); // convert input data to numeric
 
-	start_time = ((float)clock()) / CLOCKS_PER_SEC; // measuring the current time
-	
-	period = atoi(argv[1]); // convert input data to numeric
-	numOfL = atoi(argv[2]); // convertiong input data to numeric
+	start_time = (float)clock() / CLOCKS_PER_SEC; // measuring the current time
 
-	sig.sa_handler = processFunction; // setting handling function
-	sigemptyset(&sig.sa_mask); // cleaning "set" from all signals
+	sigact.sa_handler = processFunction; // setting handling function
+	sigemptyset(&sigact.sa_mask); // cleaning "set" from all signals
 	
-	sigaddset(&sig.sa_mask, SIGTSTP); // SIGTSTP signal adding to mask
-	
-	sigprocmask(SIG_BLOCK, &sig.sa_mask, NULL); // SIGTSTP signal blocking
-	sig.sa_flags = 0; // no flags added
-	
-	// !!! need to change 0 to NULL for "my code style"
-	sigaction(SIGALRM, &sig, 0); // setting responce to SIGALRM signal
+	sigaddset(&sigact.sa_mask, SIGTSTP); // SIGTSTP signal adding to mask
+	sigprocmask(SIG_BLOCK, &sigact.sa_mask, NULL); // SIGTSTP signal blocking
+	sigact.sa_flags = 0; // no flags added
+	sigaction(SIGALRM, &sigact, NULL); // setting responce to SIGALRM signal
 
-	cur.it_interval.tv_sec = period; // период перезапуска
-	cur.it_interval.tv_usec = 0;
-	cur.it_value.tv_usec = 1;
-	cur.it_value.tv_sec = 0; // первый запуск через period сек
-	setitimer(ITIMER_REAL, &cur, &old);
+	// settings for 1st launch, because time (10 us) will be left very fast
+	value.it_value.tv_sec = 0; // setting 0 seconds. only useconds
+	value.it_value.tv_usec = 10; // setting 10 useconds for fast timer counting --> faster the signal will be sended
 	
-	for (int i = 0; i < numOfL; ++i)
+	// setting new interval
+	value.it_interval.tv_sec = number_period; // interval s, launch period
+	value.it_interval.tv_usec = 0; // interval us, launch period
+	
+	setitimer(ITIMER_REAL, &value, &old_value); // ITIMER_REAL means always, 
+	
+	for (int i = 0; i < number_launch; i++)
 	{
 		pause(); // SIGALRM signal waiting
 		cout << "Parent process' work time (seconds): " << finalTime(start_time) << "\n\n";
@@ -59,21 +57,20 @@ void processFunction (int local_int) // function for repeat
 
 	if (local_pid == 0) // if child is created
 	{
-		float local_start_time = 0;
-		sigset_t set;
-		time_t seconds = time(NULL);
-		struct tm* timeinfo = localtime(&seconds);
-		sigemptyset(&set);
+		time_t local_seconds = time (NULL); // seconds since
+		sigset_t local_set; // signal set
+		struct tm* local_time = localtime (&local_seconds); // time date
+		float local_start_time = (float)clock() / CLOCKS_PER_SEC; // initilaizing & counting current time since starting program
 		
-		local_start_time = ((float)clock()) / CLOCKS_PER_SEC;
+		sigemptyset(&local_set); // setting emty signal set
 		
-		cout << "Child process' PID: " << getpid() << "\n";
-		cout << "Parent process' work start time: " << asctime(timeinfo); //<< "\n";
+		cout << "Child process' PID: " << getpid() << "\n"; // get child process' parent ID
+		cout << "Parent process' work start time: " << asctime(local_time); //<< "\n";
 
-		sigaddset(&set, SIGTSTP);
-		sigprocmask(SIG_BLOCK, &set, NULL);
+		sigaddset(&local_set, SIGTSTP); // adding signal SIGTSTP (anti ctrl-z) to the current process
+		sigprocmask(SIG_BLOCK, &local_set, NULL); // adding blocked signals to the set, SIG_BLOCK means blocked signals are current set + set argument in function
 		
-		cout << "Child process' work time (seconds): " << finalTime(local_start_time) << "\n";
+		cout << "Child process' work time (seconds): " << finalTime(local_start_time) << "\n"; // cou
 		
 		exit(EXIT_SUCCESS);
 	}
@@ -83,9 +80,9 @@ void processFunction (int local_int) // function for repeat
 	}
 }
 
-float finalTime (float local_start_time)
+float finalTime (float local_start_time) // counting current time since starting prohram & last function
 {
 	//float local_end_time = 0;
 	//local_end_time = ((float)clock()) / CLOCKS_PER_SEC);
-	return ((((float)clock()) / CLOCKS_PER_SEC) - local_start_time);
+	return (((float)clock() / CLOCKS_PER_SEC) - local_start_time);
 }
