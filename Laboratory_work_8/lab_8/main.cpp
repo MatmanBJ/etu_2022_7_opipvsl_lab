0f7c8168771101805ddcc4a17ccd7ee045f23ca2
MatmanBJ <matmanbj@gmail.com>
2022-11-13 15:33:18 +0300

    lab_8 program v2 (director's cut)

--- Laboratory_work_8/lab_8/main.cpp
+++ Laboratory_work_8/lab_8/main.cpp
@@ -7,50 +7,57 @@
 
 #include <iostream>
 #include <fstream>
-#include <string>
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/ipc.h>
 #include <sys/msg.h>
 #include <errno.h>
 #include <cstring>
+#include <string>
 
 using namespace std;
 
-void sendingRequest (struct msg_request *local_buffer, int local_other_program_id, int local_program_id, int local_local_queue, int local_common_queue);
+// https://www.opennet.ru/man.shtml?topic=msgsnd&category=2&russian=0
+// https://www.opennet.ru/man.shtml?topic=msgrcv&category=2&russian=0
 
-struct msg_request
+typedef struct // order is very important
 {
-	long mtype; //номер программы-получателя
-	int sender;	//номер программы-отправителя
-	int local_qid; //идентификатор локальной очереди
-	time_t request_time; //время посылки запроса
-};
+	long receiver_id; // program-receiver id
+	int sender_id;	// program-sender id
+	int local_queue_id; // local queue id
+	time_t request_time; // request sending time
+} MessageRequest;
 
-struct msg_response
+typedef struct
 {
-	int sender; //номер программы
-	time_t response_time; //время ответа программы, получившей запрос
-};
+	int sender_id; // program-sender id
+	time_t response_time; // responce time of program, who got request
+} MessageResponse;
+
+void sendingRequest (MessageRequest *local_buffer, int local_other_program_id, int local_program_id, int local_local_queue, int local_common_queue);
 
 int main(int argc, char *argv[])
 {
-	bool creator;
-	int local_queue; 
-	int common_queue;
-	int perm_get = 0;
-	int perm_send = 0;
-	int readers_end = 0;
-	int other_first_program_id = 0;
-	int other_second_program_id = 0;
-	struct msg_response buffer;
-	int index;
-	struct msg_request msg_req_buf[2];
-	struct msg_request req;
-	struct msg_request buf[4];
-	int prog_id = 0;
+	bool common_queue_owner; // owner of common queue
+	
+	int local_queue = 0; // local queue
+	int common_queue = 0; // common queue
+	int ability_got = 0; // ability to read got counter
+	int ability_sended = 0; // ability to read sended counter
+	int is_finished = 0; // number of program, who finished reading file
+	int other_first_program_id = 0; // other program id
+	int other_second_program_id = 0; // other program id
+	int program_id = 0; // this program id
+	int message_number = 0; // array number of recieved message
+	
+	MessageResponse message_response; // message responce to send to other programs
+	MessageRequest message_request_receive[2]; // message request to receiving from other programs
+	MessageRequest message_request; // message request
+	MessageRequest message_request_send[4]; // message request to sending to other programs
 	
 	// ---------- FLAG CHECK ----------
+	
+	// https://stackoverflow.com/questions/57929025/why-the-first-argument-of-argv-is-1-not-0
 	
 	if (strcmp(argv[1], string("-num").c_str()) != 0) // clutch!!!
 	{
@@ -80,8 +87,8 @@ int main(int argc, char *argv[])
 		}
 	}
 	
-	prog_id = atoi(argv[2]);
-	cout << "---------- PROGRAM NUMBER " << prog_id << " ----------\n";
+	program_id = atoi(argv[2]);
+	cout << "---------- PROGRAM NUMBER " << program_id << " ----------\n";
 	
 	// ---------- CREATING/OPENING COMMON QUEUE ----------
 	
@@ -92,7 +99,7 @@ int main(int argc, char *argv[])
 	// checking if common queue has been created
 	if (common_queue != -1) // if we created common queue, write message
 	{
-		creator = true;
+		common_queue_owner = true;
 		cout << "---------- COMMON QUEUE HAS BEEN CREATED ----------\n";
 	}
 	else // if we hasn't been created common queue, try to open, write message
@@ -117,7 +124,7 @@ int main(int argc, char *argv[])
 	if (local_queue == -1) // if not created -- delete remaining object if it has been created & print message
 	{
 		cout << "---------- LOCAL QUEUE HAS NOT BEEN CREATED ----------\n\n";
-		if (creator) // deleting local queue if there is remaining object
+		if (common_queue_owner == true) // deleting local queue if there is remaining object
 		{
 			msgctl(common_queue, IPC_RMID, NULL);
 		}
@@ -130,101 +137,105 @@ int main(int argc, char *argv[])
 	
 	// ---------- SENDING REQUESTS FOR ABILITY TO READ TO OTHER PROGRAMS ----------
 	
-	other_first_program_id = (prog_id)%3 + 1;
-	other_second_program_id = (prog_id+1)%3 + 1;
+	other_first_program_id = (program_id) % 3 + 1;
+	other_second_program_id = (program_id + 1) % 3 + 1;
 	
-	sendingRequest (buf, other_first_program_id, prog_id, local_queue, common_queue);
-	sendingRequest (buf, other_second_program_id, prog_id, local_queue, common_queue);
+	sendingRequest (message_request_send, other_first_program_id, program_id, local_queue, common_queue);
+	sendingRequest (message_request_send, other_second_program_id, program_id, local_queue, common_queue);
 	
 	// ---------- GETTING REQUESTS FOR ABILITY AND ABILITIES TO READ FROM OTHER PROGRAMS ----------
 	
-	while(perm_get < 2) // when we have not got abilities to read from 2 other programs
+	while(ability_got < 2) // when we have not got abilities to read from 2 other programs
 	{
-		if(msgrcv(common_queue, &msg_req_buf[index], sizeof(msg_req_buf[index]), prog_id, IPC_NOWAIT) != -1) // common queue message check
+		if(msgrcv(common_queue, &message_request_receive[message_number], sizeof(message_request_receive[message_number]), program_id, IPC_NOWAIT) != -1) // common queue message check
 		{
-			cout << "Request to read has been got from: " << msg_req_buf[index].sender << "\n";
-			cout << "Request to read has been send at: " << ctime(&msg_req_buf[index].request_time) << "\n";
+			cout << "Request to read has been got from: " << message_request_receive[message_number].sender_id << "\n";
+			cout << "Request to read has been send at: " << ctime(&message_request_receive[message_number].request_time) << "\n";
 			
 			// if the request TIME for ability to read from OTHER program <= request TIME for ability to read from THIS program,
-			// THIS program sends the ability to read to OTHER program
-			if((msg_req_buf[index].request_time < buf[msg_req_buf[index].sender].request_time)
-			  || (msg_req_buf[index].request_time == buf[msg_req_buf[index].sender].request_time 
-			  && msg_req_buf[index].sender < prog_id))
+			// THIS program sends the ability to read to OTHER program (< || (= & id_sender < id_this))
+			if((message_request_receive[message_number].request_time
+				< message_request_send[message_request_receive[message_number].sender_id].request_time)
+				|| (message_request_receive[message_number].request_time
+				== message_request_send[message_request_receive[message_number].sender_id].request_time 
+				&& message_request_receive[message_number].sender_id < program_id))
 			{
-				buffer.sender = prog_id;
-				buffer.response_time = time(NULL);
-				msgsnd(msg_req_buf[index].local_qid, &buffer, sizeof(buffer), 0);
-				perm_send = perm_send + 1;
-				cout << "Отправка разрешения на чтение для " << msg_req_buf[index].sender << "\n\n";
+				message_response.sender_id = program_id;
+				message_response.response_time = time(NULL);
+				msgsnd(message_request_receive[message_number].local_queue_id, &message_response, sizeof(message_response), 0);
+				ability_sended = ability_sended + 1;
+				
+				cout << "Sending ability to read to: " << message_request_receive[message_number].sender_id << "\n\n";
 			}
-			else // else untreated request will be placed to "msg_req_buf" array
+			else // else, untreated request will be placed to "message_request_receive" array
 			{
-				index = index + 1;
+				message_number = message_number + 1;
 			}
 		}
 		// check messages in local queue for abilities to read from other programs
-		if(msgrcv(local_queue, &buffer, sizeof(buffer), 0, IPC_NOWAIT) != -1)
+		if(msgrcv(local_queue, &message_response, sizeof(message_response), 0, IPC_NOWAIT) != -1)
 		{
-			perm_get = perm_get + 1;
-			cout << "Ability to read has been got from: " << buffer.sender << "\n";
-			cout << "Ability to read has been send at: " << ctime(&buffer.response_time) << "\n";
+			ability_got = ability_got + 1;
+			
+			cout << "Ability to read has been got from: " << message_response.sender_id << "\n";
+			cout << "Ability to read has been send at: " << ctime(&message_response.response_time) << "\n";
 		}
 	}
 	
 	// ---------- OPENING AND READING THE FILE ----------
 	
-	cout << "\n---------- OPEN FILE BEGIN ----------\n\n";
-	fstream in("input.txt");
-	string line;
-	cout << "\n---------- OPEN FILE END ----------\n\n";
+	cout << "---------- OPEN FILE BEGIN ----------\n";
+	fstream local_file(argv[4]);
+	string local_string;
+	cout << "---------- OPEN FILE END ----------\n";
 	
-	cout << "\n---------- READ FILE BEGIN ----------\n\n";
-	while(!in.eof() && getline(in, line))
+	cout << "---------- READ FILE BEGIN ----------\n";
+	while(!local_file.eof() && getline(local_file, local_string))
 	{
-		cout << line << "\n";
+		cout << local_string << "\n";
 	}
-	cout << "\n---------- READ FILE END ----------\n\n\n";
-	in.close();
+	cout << "---------- READ FILE END ----------\n\n";
+	local_file.close();
 	
 	// ---------- REQUESTS TREATMENT ----------
 	
-	while(index > 0) // all requests treatment, if they wasn't treated before
+	while(message_number > 0) // all requests treatment, if they wasn't treated before
 	{
-		buffer.sender = prog_id;
-		buffer.response_time = time(NULL);
-		msgsnd(msg_req_buf[index - 1].local_qid, &buffer, sizeof(buffer), 0);
-		perm_send++;
-		cout << "Sending ability to read for " << msg_req_buf[index - 1].sender << "\n";
-		index = index - 1;
+		message_response.sender_id = program_id;
+		message_response.response_time = time(NULL);
+		msgsnd(message_request_receive[message_number - 1].local_queue_id, &message_response, sizeof(message_response), 0);
+		ability_sended = ability_sended + 1;
+		cout << "Sending ability to read for " << message_request_receive[message_number - 1].sender_id << "\n";
+		message_number = message_number - 1;
 	}
 	
-	while(perm_send < 2) // if other program sended request before checking common queue
+	while(ability_sended < 2) // if other program sended request before checking common queue
 	{
-		if(msgrcv(common_queue, &msg_req_buf[0], sizeof(msg_req_buf[0]), prog_id, IPC_NOWAIT) != -1) // checking messages from common queue
+		if(msgrcv(common_queue, &message_request_receive[0], sizeof(message_request_receive[0]), program_id, IPC_NOWAIT) != -1) // checking messages from common queue
 		{
-			buffer.sender = prog_id;
-			buffer.response_time = time(NULL);
-			msgsnd(msg_req_buf[0].local_qid, &buffer, sizeof(buffer), 0);
-			perm_send++;
-			cout << "Sending ability to read for " << msg_req_buf[0].sender << "\n";
+			message_response.sender_id = program_id;
+			message_response.response_time = time(NULL);
+			msgsnd(message_request_receive[0].local_queue_id, &message_response, sizeof(message_response), 0);
+			ability_sended = ability_sended + 1;
+			cout << "Sending ability to read for " << message_request_receive[0].sender_id << "\n";
 		}
 	}
 	
-	req.mtype = 4; // message type -- 4
-	req.request_time = time(NULL);
-	req.local_qid = local_queue;
-	req.sender = prog_id;
-	msgsnd(common_queue, &req, sizeof(req), 0); // sending ready signal to delete common queue
+	message_request.receiver_id = 4; // message type -- 4
+	message_request.request_time = time(NULL);
+	message_request.local_queue_id = local_queue;
+	message_request.sender_id = program_id;
+	msgsnd(common_queue, &message_request, sizeof(message_request), 0); // sending ready signal to delete common queue
 	
 	// ---------- CLEANING & TERMINATING ----------
 	
-	if(creator) // waiting till other processes will finish
+	if(common_queue_owner == true) // waiting till other processes will finish
 	{
-		while(readers_end < 3)
+		while(is_finished < 3)
 		{
-	 		if(msgrcv(common_queue, &buf[0], sizeof(buf[0]), 4, 0) != -1)
+	 		if(msgrcv(common_queue, &message_request_send[0], sizeof(message_request_send[0]), 4, 0) != -1)
 			{
-				readers_end = readers_end + 1;
+				is_finished = is_finished + 1;
 			}
 	 	}
 		msgctl(common_queue, IPC_RMID, 0); // deleting common queue
@@ -234,13 +245,13 @@ int main(int argc, char *argv[])
 	return 0;
 }
 
-void sendingRequest (struct msg_request *local_buffer, int local_other_program_id, int local_program_id, int local_local_queue, int local_common_queue)
+void sendingRequest (MessageRequest *local_buffer, int local_other_program_id, int local_program_id, int local_local_queue, int local_common_queue)
 {
-	local_buffer[local_other_program_id].mtype = local_other_program_id;
+	local_buffer[local_other_program_id].receiver_id = local_other_program_id;
 	local_buffer[local_other_program_id].request_time = time(NULL);
-	local_buffer[local_other_program_id].local_qid = local_local_queue;
-	local_buffer[local_other_program_id].sender = local_program_id;
+	local_buffer[local_other_program_id].local_queue_id = local_local_queue;
+	local_buffer[local_other_program_id].sender_id = local_program_id;
 	msgsnd(local_common_queue, &local_buffer[local_other_program_id], sizeof(local_buffer[local_other_program_id]), 0);
-	cout << "Request to read has been send to: " << local_buffer[local_other_program_id].mtype << "\n";
-	cout << "Request to read has been send at: " << ctime(&local_buffer[local_other_program_id].request_time) << "\n\n";
+	cout << "Request to read has been send to: " << local_buffer[local_other_program_id].receiver_id << "\n";
+	cout << "Request to read has been send at: " << ctime(&local_buffer[local_other_program_id].request_time) << "\n";
 }
