9307009f3c1e3fb8dec192a29a12cdcbcd2c0f28
MatmanBJ <matmanbj@gmail.com>
2022-11-19 19:09:33 +0300

    lab_9 program v2 (director's & theatrical cut)

--- Laboratory_work_9/lab_9/theatrical_cut/executable_1.cpp
+++ Laboratory_work_9/lab_9/theatrical_cut/executable_1.cpp
@@ -1,20 +1,11 @@
 /*
- * ./main -id (0 | 1 | 2) -time interval_time -num number_of_strings -file *.txt -key shared_mem_seg_key_value
+ * ./executable_1 interval_time number_of_strings
  *
- * -id
- *     Program (process) ID. It can be only 0, 1 or 2.
- * -time
+ * interval_time
  *     Interval time for every loop (cycle), i.e. how many times we will wait after start new iteration. Integer number in the range [-1; +inf].
- * -num
+ * number_of_strings
  *     Number of loops (cycles), i.e. how many times program will write strings in the file. Integer number in the range [0; +inf].
- * -file
- *     Filename for 3 programs to write string, with .txt extension.
- * -key
- *     Key number for shared memory segment, if 0 = IPC_PRIVATE, it won't work (probably). Integer number in the range [0; +inf].
  *
- * ./main -id 0 -time 5 -num 5 -file shared_file.txt -key 190
- * ./main -id 1 -time 2 -num 3 -file shared_file.txt -key 190
- * ./main -id 2 -time 2 -num 5 -file shared_file.txt -key 190
  */
 
 #include <iostream>
@@ -25,35 +16,6 @@
 #include <sys/shm.h>
 
 using namespace std;
-
-bool hasEnding (string const &fullString, string const &ending);
-
-class ArgFlagParse
-{
-public:
-	ArgFlagParse();
-	ArgFlagParse(int local_flags_number, string* local_flags_array);
-	~ArgFlagParse();
-	void ArgvCommonCheck (int local_argc, char* local_argv[]);
-	void ArgvCommonCheck (int local_argc, char* local_argv[], string* local_flags);
-	bool ArgvConditionCheck (int local_flag, char* local_argv);
-	bool ArgvConditionCheckID (char* local_argv);
-	bool ArgvConditionCheckTime (char* local_argv);
-	bool ArgvConditionCheckNum (char* local_argv);
-	bool ArgvConditionCheckFile (char* local_argv);
-	bool ArgvConditionCheckKey (char* local_argv);
-	bool getErrorExistance ();
-	string getErrorMessage ();
-	int getFlagsNumber ();
-	void setFlagsNumber (int local_flags_number);
-	string* getFlagsArray ();
-	void setFlagsArray (string* local_flags_array);
-private:
-	bool error_existance;
-	string error_message;
-	int flags_number;
-	string* flags_array;
-};
 
 typedef struct // struct for lamport algorithm
 {
@@ -66,30 +28,27 @@ int main (int argc, char *argv[])
 {
 	// ---------- PREPARING ----------
 	
-	string flags [] = {"-id", "-time", "-num", "-file", "-key"}; // automatic size
-	ArgFlagParse* arguments = new ArgFlagParse();
-	arguments->ArgvCommonCheck(argc, argv, flags);
-	
-	if (arguments->getErrorExistance() == true)
+	if (/*argv[1] == nullptr || */argv[2] == nullptr)
 	{
-		cout << arguments->getErrorMessage() << "\n";
+		cout << "Syntax error. Not enough arguments, must be 2: \"./executable_1 interval_time number_of_strings\"!";
+		exit(-1);
+	}
+	if (atoi(argv[1]) < 1)
+	{
+		cout << "Syntax error. Interval time to write file argument must be in range [1; +inf)!";
+		exit(-1);
+	}
+	if (atoi(argv[2]) < 1)
+	{
+		cout << "Syntax error. Number of strings to write file argument must be in range [1; +inf)!";
 		exit(-1);
 	}
 	
-	delete arguments;
-	
-	// https://stackoverflow.com/questions/1195675/convert-a-char-to-stdstring
-	int program_id = atoi(argv[2]); // program id/number
-	int interval_time = atoi(argv[4]); // interval time to wait before next start
-	int number_of_strings = atoi(argv[6]); // number of strings to write in the file
-	int key = atoi(argv[10]); // key number for shared memory segment
-	string filename = string(argv[8]); // name of the file to write strings
-	
-	//int program_id = atoi(argv[1]); // program id/number
-	//int interval_time = atoi(argv[2]); // interval time to wait before next start
-	//int number_of_strings = atoi(argv[3]); // number of strings to write in the file
-	//int key = 190; // key number for shared memory segment
-	//string filename = "shared_file.txt"; // name of the file to write strings
+	int program_id = 1; // program id/number
+	int interval_time = atoi(argv[1]); // interval time to wait before next start
+	int number_of_strings = atoi(argv[2]); // number of strings to write in the file
+	int key = 190; // key number for shared memory segment
+	string filename = "shared_file.txt"; // name of the file to write strings
 	
 	bool shared_mem_seg_owner; // is this process is owner of the shared memory segment (to free it at the end)
 	int shared_mem_seg_ptr; // pointer to the shared memory segment
@@ -245,189 +204,4 @@ int main (int argc, char *argv[])
 	 */
 	
 	return 0;
-}
-
-// ---------- FUNCTIONS ----------
-
-// https://stackoverflow.com/questions/874134/find-out-if-string-ends-with-another-string-in-c
-bool hasEnding (string const &fullString, string const &ending)
-{
-    if (fullString.length() >= ending.length())
-    {
-        return (0 == fullString.compare (fullString.length() - ending.length(), ending.length(), ending));
-    }
-    else
-    {
-        return false;
-    }
-}
-
-// ---------- CONSTRUCTOR ----------
-
-ArgFlagParse::ArgFlagParse()
-{
-	this->error_existance = false;
-	this->flags_number = 0;
-	this->flags_array = nullptr;
-}
-
-// ---------- CONSTRUCTOR CUSTOM ----------
-
-ArgFlagParse::ArgFlagParse(int local_flags_number, string* local_flags_array)
-{
-	this->error_existance = false;
-	this->flags_number = local_flags_number;
-	this->flags_array = local_flags_array;
-}
-
-// ---------- DESTRUCTOR ----------
-
-ArgFlagParse::~ArgFlagParse()
-{
-	//delete [] this->flags_array;
-}
-
-// ---------- ArgFlagParsevoid ----------
-
-void ArgFlagParse::ArgvCommonCheck (int local_argc, char* local_argv[])
-{
-	this->ArgvCommonCheck(local_argc, local_argv, this->flags_array);
-}
-
-// ---------- ArgFlagParsevoid ----------
-
-void ArgFlagParse::ArgvCommonCheck (int local_argc, char* local_argv[], string* local_flags)
-{
-	this->error_existance = false;
-	if (local_argc == 1)
-	{
-		this->error_existance = true;
-	}
-	else
-	{
-		int i = 1; // argc/argv number -- 2 by 1 loop
-		int j = 0; // flags number -- 1 by 1 loop
-		while (i < local_argc && this->error_existance == false)
-		{
-			//if (strcmp(argv[i], string("-num").c_str()) != 0) // clutch!!!
-			if (strcmp(local_argv[i], local_flags[j].c_str()) != 0) // clutch!!!
-			{
-				this->error_message = "Couldn't find \"" + local_flags[j] + "\" flag. Program terminating!";
-				this->error_existance = true;
-			}
-			else
-			{
-				if (ArgvConditionCheck (i, local_argv[i + 1]) == false) // if not satisfied
-				{
-					this->error_message = "\"" + local_flags[j] + "\" parameter doesn't satisfy the condition: " + this->error_message + "Program terminating!";
-					this->error_existance = true;
-				}
-			}
-			i = i + 2;
-			j = j + 1;
-		}
-	}
-}
-
-// ---------- ArgvConitionCheck ----------
-
-bool ArgFlagParse::ArgvConditionCheck (int local_flag, char* local_argv)
-{
-	bool local_return = true;
-	if (local_flag == 1)
-	{
-		local_return = ArgvConditionCheckID (local_argv);
-	}
-	else if (local_flag == 3)
-	{
-		local_return = ArgvConditionCheckTime (local_argv);
-	}
-	else if (local_flag == 5)
-	{
-		local_return = ArgvConditionCheckNum (local_argv);
-	}
-	else if (local_flag == 7)
-	{
-		local_return = ArgvConditionCheckFile (local_argv);
-	}
-	else if (local_flag == 9)
-	{
-		local_return = ArgvConditionCheckKey (local_argv);
-	}
-	return local_return;
-}
-
-bool ArgFlagParse::ArgvConditionCheckID (char* local_argv) // clutch -- only to be here, need to be redefined or updated
-{
-	bool local_return = true;
-	if (atoi(local_argv) != 0 && atoi(local_argv) != 1 && atoi(local_argv) != 2)
-	{
-		this->error_message = "parameter must be in range [0; 2]. ";
-		local_return = false;
-	}
-	return local_return;
-}
-
-bool ArgFlagParse::ArgvConditionCheckTime (char* local_argv) // clutch -- only to be here, need to be redefined or updated
-{
-	bool local_return = true;
-	if (atoi(local_argv) < -1)
-	{
-		this->error_message = "parameter must be in range [-1; +inf). ";
-		local_return = false;
-	}
-	return local_return;
-}
-
-bool ArgFlagParse::ArgvConditionCheckNum (char* local_argv) // clutch -- only to be here, need to be redefined or updated
-{
-	bool local_return = true;
-	if (atoi(local_argv) < 0)
-	{
-		this->error_message = "parameter must be in range [0; +inf). ";
-		local_return = false;
-	}
-	return local_return;
-}
-
-bool ArgFlagParse::ArgvConditionCheckFile (char* local_argv) // clutch -- only to be here, need to be redefined or updated
-{
-	bool local_return = true;
-	if (local_argv == nullptr)
-	{
-		this->error_message = "filename hasn't been found. ";
-		local_return = false;
-	}
-	// https://stackoverflow.com/questions/874134/find-out-if-string-ends-with-another-string-in-c
-	else if (hasEnding(string(local_argv), ".txt") == false)
-	{
-		this->error_message = "filename must contain \".txt\" extension. ";
-		local_return = false;
-	}
-	return local_return;
-}
-
-bool ArgFlagParse::ArgvConditionCheckKey (char* local_argv) // clutch -- only to be here, need to be redefined or updated
-{
-	bool local_return = true;
-	if (atoi(local_argv) < 0)
-	{
-		this->error_message = "parameter must be in range [0; +inf). ";
-		local_return = false;
-	}
-	return local_return;
-}
-
-// ---------- getErrorExistance ----------
-
-bool ArgFlagParse::getErrorExistance ()
-{
-	return this->error_existance;
-}
-
-// ---------- getErrorMessage ----------
-
-string ArgFlagParse::getErrorMessage ()
-{
-	return this->error_message;
 }
